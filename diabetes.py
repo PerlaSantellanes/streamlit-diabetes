cb0126147b54e9c90501aef84d80a5edfa5be8da
PerlaSantellanes <100666648+PerlaSantellanes@users.noreply.github.com>
2022-12-02 20:53:14 -0700

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -1,8 +1,20 @@
 import numpy as np
 import pickle
 import streamlit as st
-from PyInstaller.utils.hooks import copy_metadata
-datas = copy_metadata('streamlit')
+
+st.session_state['answer'] = ''!
+
+st.write(st.session_state)
+
+realans = ['', 'abc', 'edf']
+
+if  st.session_state['answer'] in realans:
+    answerStat = "correct"
+elif st.session_state['answer'] not in realans:
+    answerStat = "incorrect"
+
+st.write(st.session_state)
+st.write(answerStat)
 
 # Load the saved model
 loaded_model = pickle.load(open('trained_model.sav', 'rb'))
