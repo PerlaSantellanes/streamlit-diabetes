954016e26553473f8d6cfb5e91b77f26ccaf5c7e
PerlaSantellanes <100666648+PerlaSantellanes@users.noreply.github.com>
2022-12-02 21:05:40 -0700

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -1,9 +1,8 @@
 
-import warnings
 import numpy as np
 import pickle
 import streamlit as st
-warnings.filterwarnings("ignore")
+
 
 # Load the saved model
 loaded_model = pickle.load(open("trained_model.sav", "rb"))
